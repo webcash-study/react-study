7584b66bbf67948d6549365dc9cbd1e699fec051
chygai232 <chygai232@gmail.com>
2025-07-04 13:41:32 +0900

    prj: useState를 이용하여 할 일 후가 및 상태 관리 구현

--- front-end/React/chap04/first-todo-list/src/components/Controls.jsx
+++ front-end/React/chap04/first-todo-list/src/components/Controls.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import "./Controls.css";
 
-function Controls() {
+function Controls({ onSubmit, filterType, onChangeFilterType }) {
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = () => {
+    onSubmit(text);
+    setText("");
+  };
+
   return (
     <div className="controls">
-      <input type="text" className="input" />
-      <button className="button">추가</button>
-      <select name="select" className="select">
-        <option value="">전체</option>
-        <option value="">할 일</option>
-        <option value="">완료</option>
+      <input
+        type="text"
+        className="input"
+        value={text}
+        onChange={handleChange}
+      />
+      <button className="button" onClick={handleSubmit}>
+        추가
+      </button>
+      <select
+        name="select"
+        className="select"
+        value={filterType}
+        onChange={(e) => onChangeFilterType(e.target.value)}
+      >
+        <option value="ALL">전체</option>
+        <option value="TODO">할 일</option>
+        <option value="COMPLETED">완료</option>
       </select>
     </div>
   );

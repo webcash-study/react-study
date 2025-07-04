4a1792ea48f91721ff4c743f3dd07f8d329257cd
chygai232 <chygai232@gmail.com>
2025-07-04 09:58:10 +0900

    docs: virtual dom과 렌더링(react reconciliation)

---
+++ front-end/React/chap03/ex05/src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * <div><Counter /></div>
+ *
+ * <span></span>
+ *
+ * <div style={{background: 'red'}}>A</div>
+ * <div style={{background: 'blue'}}>A</div>
+ *
+ * <Counter number={1} />
+ * <Counter number={2} />
+ */
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;

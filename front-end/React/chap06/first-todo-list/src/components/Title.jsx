9f7371fb68febdb9dd8ea1dab85fa59539547b91
chygai232 <chygai232@gmail.com>
2025-07-14 15:55:43 +0900

    prj: Redux Toolkit 소개 및 Todo 적용

---
+++ front-end/React/chap06/first-todo-list/src/components/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+  return <h1 className="text-2xl mb-[16px]">To-Do List</h1>;
+}
+
+export default Title;
---
+++ front-end/React/chap06/first-todo-list/src/components/TodoItem.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggleTodo, updateTodo } from "../store/todoSlice";
+
+const itemClassName = `
+  flex items-center h-[65px] gap-[12px] px-[12px] py-[0px]
+`;
+
+const checkboxClassName = `w-[16px] h-[16px]`;
+
+const textClassName = `grow`;
+
+const buttonClassName = `bg-black text-white border-none`;
+
+const inputClassName = `
+  grow border-[1px] border-solid border-gray-500 rounded-[6px]
+  bg-transparent px-[12px] py-[4px] text-[14px] leading-[20px] text-white
+`;
+
+function TodoItem({ id, text, completed }) {
+  const dispatch = useDispatch();
+  const [edit, setEdit] = useState(false);
+  const handleEdit = () => {
+    setEdit((prev) => !prev);
+  };
+  const handleChange = (e) => {
+    dispatch(updateTodo({ id, text: e.target.value }));
+  };
+  const handleToggle = () => {
+    dispatch(toggleTodo(id));
+  };
+  const handleDelete = () => {
+    dispatch(deleteTodo(id));
+  };
+  return (
+    <div className={itemClassName}>
+      <input
+        className={checkboxClassName}
+        type="checkbox"
+        checked={completed}
+        onChange={handleToggle}
+      />
+      {edit ? (
+        <input
+          className={inputClassName}
+          value={text}
+          onChange={handleChange}
+        />
+      ) : (
+        <p
+          className={[textClassName, completed && "line-through"].join(" ")}
+          completed={completed}
+        >
+          {text}
+        </p>
+      )}
+      <button className={buttonClassName} onClick={handleEdit}>
+        수정
+      </button>
+      <button className={buttonClassName} onClick={handleDelete}>
+        삭제
+      </button>
+    </div>
+  );
+}
+
+export default TodoItem;
